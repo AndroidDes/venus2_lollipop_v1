836765d779ed9f221620b7ddef446eed587800ac
Desterly <d3s7royer@gmail.com>
2012-06-22 19:03:31 -0400

    Initial Commit

---
+++ BoardConfig.mk
@@ -0,0 +1,40 @@
+# Droid Pro Board Config
+
+# Use the non-open-source part, if present
+-include vendor/motorola/droid2we/BoardConfigVendor.mk
+
+# Use the part that is common between all tunas
+include device/motorola/omap34com/BoardConfig.mk
+
+# Processor
+TARGET_BOOTLOADER_BOARD_NAME := venus2
+
+# Command line
+TARGET_PREBUILT_KERNEL := device/motorola/venus2/kernel
+BOARD_KERNEL_CMDLINE := console=ttyS2,115200n8 rw mem=498M@0x80C00000 init=/init ip=off brdrev=P3A androidboot.bootloader=0x0000 mmcparts=mmcblk1:p7(pds),p15(boot),p16(recovery),p17(cdrom),p18(misc),p19(cid),p20(kpanic),p21(system),p22(cache),p23(preinstall),p24(userdata) androidboot.mode=reboot androidboot.bootloader=D011 androidboot.serialno=0A3A94CF0602D02
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_AUDIO_LEGACY := true
+ifdef BOARD_USES_AUDIO_LEGACY
+COMMON_GLOBAL_CFLAGS += -DBOARD_USES_AUDIO_LEGACY
+endif
+TARGET_PROVIDES_LIBAUDIO := true
+
+COMMON_GLOBAL_CFLAGS += -DUSE_TI_COMMANDS
+
+# Camera
+BOARD_OVERLAY_BASED_CAMERA_HAL := true
+
+# Recovery
+TARGET_PREBUILT_RECOVERY_KERNEL := device/motorola/venus2/kernel
+BOARD_MKE2FS := device/motorola/omap34com/releaseutils/mke2fs
+
+TARGET_PROVIDES_INIT_RC := true
+
+# UMS
+BOARD_MASS_STORAGE_FILE_PATH := "/sys/devices/platform/usb_mass_storage/lun0/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/usb_mass_storage/lun0/file"
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+BOARD_CUSTOM_USB_CONTROLLER := ../../device/motorola/droid2we/UsbController.cpp
+BOARD_MTP_DEVICE := "/dev/mtp"
---
+++ cm.mk
@@ -0,0 +1,73 @@
+# This is the product configuration for a full venus2
+DEVICE_PREBUILT := device/motorola/venus2/prebuilt
+
+# high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+# Camera
+#PRODUCT_PACKAGES := \
+#	Camera
+
+# Audio
+PRODUCT_COPY_FILES += \
+    device/motorola/venus2/audio/libaudio.so:/system/lib/libaudio.so \
+    device/motorola/venus2/audio/liba2dp.so:/system/lib/liba2dp.so
+
+PRODUCT_PACKAGES += \
+    sensors.venus2 \
+    audio.primary.omap3 \
+    libaudiohw_legacy \
+    audio.a2dp.default \
+    libaudioutils \
+    hcitool \
+    hciattach \
+    hcidump
+
+PRODUCT_PACKAGES += \
+    hijack-boot.zip
+
+# Hijack files
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PREBUILT)/etc/hijack-boot.zip:system/etc/hijack-boot.zip
+
+# Keymaps
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PREBUILT)/usr/keychars/sholes-keypad.kcm:system/usr/keychars/sholes-keypad.kcm \
+	$(DEVICE_PREBUILT)/usr/keylayout/sholes-keypad.kl:system/usr/keylayout/sholes-keypad.kl
+
+# Kernel
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/motorola/venus2/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# copy all kernel modules under the "modules" directory to system/lib/modules
+PRODUCT_COPY_FILES += $(shell \
+    find device/motorola/venus2/modules -name '*.ko' \
+    | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
+    | tr '\n' ' ')
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.ril.features=0x0E \
+	persist.ril.modem.ttydevice=/dev/usb/tty1-3:1.4 \
+	persist.ril.mux.noofchannels=8 \
+	ro.ril.ntmodeglobal=true
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/motorola/omap34com/full_base_telephony.mk)
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+# Inherit from maguro device
+$(call inherit-product, device/motorola/venus2/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := venus2
+PRODUCT_NAME := cm_venus2
+PRODUCT_BRAND := verizon
+PRODUCT_MODEL := DROID Pro
+PRODUCT_MANUFACTURER := Motorola
